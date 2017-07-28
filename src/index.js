46517d642c52fd6066ac14c4c10561f3ac61b09d
Jacob Erling <jacobs_macbook@Jacobs-MacBook-Pro.local>
2017-07-28 11:46:22 -0400

    index.js update

--- src/index.js
+++ src/index.js
@@ -2,27 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Main from './components/Main';
-import Header from './components/Header';
-import Background from './components/Background';
-import Profile from './components/Profile';
-import Intro from './components/Intro';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Network from './components/Network';
-import Contact from './components/Contact';
 
 
 ReactDOM.render(
-	<Main>
-		<Header />
-		<Background>
-			<Profile>
-				<Intro />
-				<Skills />
-				<Projects />
-				<Network />
-				<Contact />
-			</Profile>
-		</Background>
-	</Main>, 
+	<Main /> , 
 	document.getElementById('root'));
